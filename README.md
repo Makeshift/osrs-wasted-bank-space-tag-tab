fe555f38735ab35b2d8aba4c041f513761094dda
Connor Bell (Makeshift) <git@connor-bell.com>
2024-08-17 16:43:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# osrs-wasted-bank-space-tag-tab
+
+## What is
+
+This is a terrible script that parses [mcgeer/WastedBankSpace](https://github.com/mcgeer/WastedBankSpace)'s source code to generate a bank tag layout compatible the RuneLite clients' tag tabs.
+
+## How to use in RuneLite
+
+- Navigate to [this link]() for the most recent generated version.
+- Copy the entire thing
+- Open your bank in OSRS, right click the `+` button at the top left and select `Import tag tab`
+![import tag tab button](import.png)
+
+If you aren't interested in the layout and simply want a tab containing only the wasted bank slots, right-click the tab and select `Delete layout`
+![delete layout button](delete-layout.png)
+
+## Development / Manual Generation
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
---
+++ index.ts
@@ -0,0 +1,104 @@
+import { Glob } from 'bun'
+import * as fs from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { basename, join } from 'node:path'
+import Git from 'simple-git'
+
+const wbsGit = Git()
+const rlGit = Git()
+
+const wbsRelativeLocationsDir = join(...'src/main/java/com/wastedbankspace/model/locations/'.split('/'))
+const rlRelativeItemsFile = join(...'runelite-api/src/main/java/net/runelite/api/ItemID.java'.split('/'))
+
+let wbsDir: string
+let rlDir: string
+
+const cloneOpts = {
+  '--depth': 1
+}
+
+if (process.env.NODE_ENV === 'development') {
+  wbsDir = join(import.meta.dir, 'wasted-bank-space')
+  rlDir = join(import.meta.dir, 'runelite')
+} else {
+  wbsDir = await fs.mkdtemp(join(tmpdir(), 'wasted-bank-space-'))
+  rlDir = await fs.mkdtemp(join(tmpdir(), 'runelite-'))
+}
+
+const wbsLocationsDir = join(wbsDir, wbsRelativeLocationsDir)
+const rlItemsFile = join(rlDir, rlRelativeItemsFile)
+
+console.log('WBS clone dir:', wbsDir)
+console.log('RL clone dir:', rlDir)
+
+if (
+  // Hopefully the name doesn't change, I guess. Too lazy to check for the dir because Bun doesn't have
+  //  a native way to do it yet.
+  !await fs.exists(wbsLocationsDir)
+  || !await fs.exists(rlItemsFile)
+) {
+  console.log('Cloning repositories...')
+  await Promise.all([
+    wbsGit.clone('https://github.com/mcgeer/WastedBankSpace.git', wbsDir, cloneOpts),
+    rlGit.clone('https://github.com/runelite/runelite.git', rlDir, cloneOpts)
+  ])
+}
+
+// Parsing java source code with regular expressions :D This is so fragile
+
+const categoriseWbsItems = async (dir: string) => {
+  const glob = new Glob('**/*.java')
+  const matches = await glob.scan(dir)
+  const results: Record<string, string[]> = {}
+  for await (const match of matches) {
+    const category = basename(match).replace('.java', '')
+
+    const content = await Bun.file(join(wbsLocationsDir, match)).text()
+    const items = [...content.matchAll(/ItemID\.(\w+)/g)].map(match => match[1])
+    results[category] = items
+  }
+  return results
+}
+
+const mapRlItems = async (file: string) => {
+  const content = await Bun.file(file).text()
+  const items = [...content.matchAll(/(\w+) = (\d+);/g)].map(match =>
+    ([match[1], match[2]]))
+  return Object.fromEntries(items)
+}
+
+const [ categorisedItems, itemMappings ] = await Promise.all([
+  categoriseWbsItems(join(wbsDir, wbsRelativeLocationsDir)),
+  mapRlItems(join(rlDir, rlRelativeItemsFile))
+])
+
+console.log(`Got ${Object.keys(categorisedItems).length} categories and ${Object.keys(itemMappings).length} total items`)
+
+const categorisedItemIds = Object.fromEntries(
+  Object.entries(categorisedItems)
+    .map(([category, items]) => {
+      const itemIds = items.map(item => itemMappings[item])
+      return [category, itemIds]
+    })
+)
+const allItemIds = Object.values(categorisedItemIds).flat()
+
+let generatedLayout = 'banktaglayoutsplugin:wastedbankslots,'
+
+let currentSlot = 0
+for (const [, itemIds] of Object.entries(categorisedItemIds)) {
+  for (const itemId of itemIds) {
+    generatedLayout += `${itemId}:${currentSlot},`
+    currentSlot++
+  }
+  // Bank has 8 slots per row
+  // We want to skip the rest of the current row and the whole next row
+  currentSlot += (16 - (currentSlot % 8))
+}
+
+// Handle runelite vanilla with no layout data
+generatedLayout += 'banktag:wastedbankslots,1038,' + allItemIds.join(',')
+
+await Bun.write('generated-tab.txt', generatedLayout)
+
+console.log('Done!')
